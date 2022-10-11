8a48ac1adf3cc0ed1c635cb4429aeafcec788737
AndrewBurUU <andriv80@gmail.com>
2022-10-11 18:45:48 +0800

    2.1. Преобразование даты [#289476 #350663]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,13 +1,12 @@
 package ru.job4j.grabber.utils;
 
 import java.time.*;
-import java.time.format.*;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse.substring(0, 19));
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 
 }
