73f7d2046f35c8b05227403174fad937be6bf980
AndrewBurUU <andriv80@gmail.com>
2022-10-11 08:16:49 +0800

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,89 @@
+package ru.job4j.grabber;
+
+import java.time.*;
+import java.util.*;
+
+/**
+ *  - id типа int - идентификатор вакансии (берется из нашей базы данных);
+ *  - title типа String - название вакансии;
+ *  - link типа String - ссылка на описание вакансии;
+ *  - description типа String - описание вакансии;
+ *  - created типа LocalDateTime - дата создания вакансии.
+ */
+public class Post {
+
+    private int id;
+
+    private String name;
+
+    private String link;
+
+    private String description;
+
+    private LocalDateTime created;
+
+    public Post(int id, String name, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(name, post.name) && Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, created);
+    }
+}
