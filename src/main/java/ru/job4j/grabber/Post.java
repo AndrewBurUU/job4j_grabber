80071f850200501ba9b68d6627f2c72656b063da
AndrewBurUU <andriv80@gmail.com>
2022-10-12 18:17:23 +0800

    6. Grabber. [#289477]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -27,17 +27,9 @@ public class Post {
 
     public Post(String name, String link, String description, LocalDateTime created) {
         this.name = name;
-        if (link.length() > 255) {
-            this.link = link.substring(0, 255);
-        } else {
-            this.link = link;
-        }
-        if (description.length() > 255) {
-            this.description = description.substring(0, 255);
-        } else {
-            this.description = description;
-        }
-        this.created = created;
+        this.link = link;
+        this.description = description;
+         this.created = created;
     }
 
     public Post(int id, String name, String link, String description, LocalDateTime created) {
